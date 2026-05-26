9600c69048d5a374305942e594048374d0e27dcf
WizzyWodich <wizzywodich@gmail.com>
2026-05-26 23:31:38 +0300

    add: enp user/, dto

---
+++ backend/Todo.Core/Endpoints/User/GetUser/GetUserEndpoint.cs
@@ -0,0 +1,18 @@
+using System.Security.Claims;
+using Todo.Core.EndpointSettings;
+
+namespace Todo.Core.Endpoints.User.GetUser;
+
+public class GetUserEndpoint : IEndpoint
+{
+    public void MapEndpoint(WebApplication app)
+    {
+        app.MapGet("/api/user", async (GetUserHandler handler, ClaimsPrincipal user, CancellationToken ct) =>
+        {
+            return await handler.HandleAsync(user, ct);
+        })
+        .WithName("GetUser")
+        .WithTags("Users")
+        .RequireAuthorization();
+    }
+}
