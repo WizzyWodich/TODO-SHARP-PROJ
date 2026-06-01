29533cab28066debd6b15f87d861364f32bfad7b
WizzyWodich <wizzywodich@gmail.com>
2026-06-01 21:58:14 +0300

    update: merge file frontend

--- frontend/src/api/task.ts
+++
@@ -1,5 +0,0 @@
-import { apiRequest } from "./api";
-
-export function getTaskList(query: any) {
-    return apiRequest("/api/task/list", query);
-}
--- frontend/src/api/user.ts
+++
@@ -1,7 +0,0 @@
-import { apiRequest } from "./api";
-import type { User } from "../types/userType";
-
-export async function getMe() : Promise<User> {
-    return await apiRequest("/user/me");
-}
-
